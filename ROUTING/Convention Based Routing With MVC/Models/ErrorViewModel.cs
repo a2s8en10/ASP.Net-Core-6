fa1d4ac8b9e5d7e1313a2859fea6d9b36c92b2d6
Anurag Sahu <anu.sahu9589@gmail.com>
2024-05-07 23:18:17 +0530

    Add code
    ROUTING

---
+++ ROUTING/Convention Based Routing With MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Convention_Based_Routing_With_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
